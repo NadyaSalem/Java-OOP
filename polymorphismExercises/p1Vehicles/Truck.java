59b6123d9013e62447f55eb40ef4e6b7e2dd38b9
Nadya Salem <nadya_salem@abv.bg>
2022-07-30 22:08:23 +0300

    Update Truck.java

--- polymorphismExercises/p1Vehicles/Truck.java
+++ polymorphismExercises/p1Vehicles/Truck.java
@@ -3,9 +3,17 @@ package oop.polymorphismExercises.p1Vehicles;
 public class Truck extends Vehicle {
 
     private final double INCREASING_fUEL_CONSUMPTION_IN_SUMMER = 1.6;
+    private final double PERCENTAGE_fUEL_LEFT_AFTER_LEAKING = 0.95;
 
     public Truck(double fuelQuantity, double fuelConsumption) {
         super(fuelQuantity, fuelConsumption);
         this.fuelConsumption = fuelConsumption + INCREASING_fUEL_CONSUMPTION_IN_SUMMER;
+    }
+
+    @Override
+    public void refueling(double liters) {
+
+        liters *= PERCENTAGE_fUEL_LEFT_AFTER_LEAKING;;
+        super.refueling(liters);
     }
 }
